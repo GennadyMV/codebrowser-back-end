717a10cac5162e8949151bc19b70aedb688dfab2
ljleppan <leo.leppanen@helsinki.fi>
2014-08-13 11:28:05 +0300

    Removed logic that now exists in the snapshot API

--- src/main/java/fi/helsinki/cs/codebrowser/model/Snapshot.java
+++ src/main/java/fi/helsinki/cs/codebrowser/model/Snapshot.java
@@ -2,17 +2,7 @@ package fi.helsinki.cs.codebrowser.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import difflib.DiffUtils;
-import difflib.Patch;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.Date;
-import java.util.LinkedList;
 import java.util.List;
 
 import javax.persistence.Entity;
@@ -117,99 +107,5 @@ public class Snapshot extends AbstractNamedPersistable implements Comparable<Sna
     public int compareTo(final Snapshot o) {
 
         return this.snapshotTime.compareTo(o.snapshotTime);
-    }
-
-    public static List<Snapshot> filterSequentialUnalteredSnapshots(final List<Snapshot> snapshots) {
-
-        if (snapshots == null || snapshots.size() <= 1) {
-            return snapshots;
-        }
-
-        Collections.sort(snapshots);
-
-        final List<Snapshot> filteredSnapshots = new ArrayList<>();
-        filteredSnapshots.add(snapshots.get(0));
-
-
-        List<String> snapshotLines = filesToLines(snapshots.get(0).getFiles());
-
-        if (snapshotsAreEqual(snapshotLines, filesToLines(snapshots.get(snapshots.size() - 1).getFiles()))) {
-            snapshots.remove(snapshots.size() - 1);
-        }
-
-        for (int i = 1; i < snapshots.size(); i++) {
-            final Snapshot current = snapshots.get(i);
-            final List<String> currentLines = filesToLines(current.getFiles());
-
-            if (snapshotsAreEqual(snapshotLines, currentLines)) {
-                continue;
-            }
-
-            filteredSnapshots.add(current);
-            snapshotLines = currentLines;
-        }
-
-        return filteredSnapshots;
-    }
-
-    private static boolean snapshotsAreEqual(final List<String> snapshotLines, final List<String> currentLines) {
-
-        if (snapshotLines.size() != currentLines.size()) {
-            return false;
-        }
-
-        final Patch diff = DiffUtils.diff(snapshotLines, currentLines);
-
-        if (diff == null || diff.getDeltas() == null || diff.getDeltas().isEmpty()) {
-            return true;
-        }
-
-        for (int i = 0; i < currentLines.size(); i++) {
-            if (currentLines.get(i).contains("/home/group")) {
-                // ignore file location
-                continue;
-            }
-
-            if (!currentLines.get(i).trim().equals(snapshotLines.get(i).trim())) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    private static List<String> filesToLines(final List<SnapshotFile> files) {
-
-        Collections.sort(files, new Comparator<SnapshotFile>() {
-            @Override
-            public int compare(final SnapshotFile o1, final SnapshotFile o2) {
-                return o1.getFilepath().compareTo(o2.getFilepath());
-            }
-        });
-
-        final List<String> lines = new ArrayList<>();
-        for (SnapshotFile file : files) {
-//            lines.add(file.getFilepath());
-            lines.addAll(fileToLines(file.getFilepath()));
-        }
-
-        return lines;
-    }
-
-    private static List<String> fileToLines(final String filename) {
-
-        final List<String> lines = new LinkedList<>();
-        String line;
-
-        try (BufferedReader in = new BufferedReader(new FileReader(filename))) {
-            while ((line = in.readLine()) != null) {
-                lines.add(line);
-            }
-            in.close();
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-
-        return lines;
     }
 }
