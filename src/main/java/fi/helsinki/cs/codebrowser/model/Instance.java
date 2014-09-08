a9275a3e1590d5fa1e681be70ca290fc0cd3a9dd
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2014-09-08 13:42:56 +0300

    Add name for instance

--- src/main/java/fi/helsinki/cs/codebrowser/model/Instance.java
+++ src/main/java/fi/helsinki/cs/codebrowser/model/Instance.java
@@ -6,15 +6,22 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public final class Instance {
 
     private final String id;
+    private final String name;
 
     @JsonCreator
-    public Instance(@JsonProperty("id") final String id) {
+    public Instance(@JsonProperty("id") final String id, @JsonProperty("name") final String name) {
 
         this.id = id;
+        this.name = name;
     }
 
     public String getId() {
 
         return id;
+    }
+
+    public String getName() {
+
+        return name;
     }
 }
