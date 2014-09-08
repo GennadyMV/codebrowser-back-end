6e8410ab221d4f37abd46c7e3a62e84470a148b1
ljleppan <leo.leppanen@helsinki.fi>
2014-09-08 14:51:43 +0300

    Tests for Instance

---
+++ src/test/java/fi/helsinki/cs/codebrowser/model/InstanceTest.java
@@ -0,0 +1,28 @@
+package fi.helsinki.cs.codebrowser.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class InstanceTest {
+
+    private static final String NAME = "TestInstanceName";
+    private static final String ID = "TestInstanceID";
+
+    private Instance instance;
+
+    @Before
+    public void setUp() {
+        
+        instance = new Instance(ID, NAME);
+    }
+
+    @Test
+    public void constructorSetsCorrectValues() {
+
+        assertEquals(NAME, instance.getName());
+        assertEquals(ID, instance.getId());
+    }
+
+}
