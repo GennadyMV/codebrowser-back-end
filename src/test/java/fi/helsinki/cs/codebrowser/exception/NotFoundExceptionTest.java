36a0cdc35da2084c2582954ef8c949c05465f7bd
ljleppan <leo.leppanen@helsinki.fi>
2014-09-08 14:50:49 +0300

    Tests for NotFoundException

---
+++ src/test/java/fi/helsinki/cs/codebrowser/exception/NotFoundExceptionTest.java
@@ -0,0 +1,13 @@
+package fi.helsinki.cs.codebrowser.exception;
+
+import org.junit.Test;
+
+public class NotFoundExceptionTest {
+
+    @Test
+    public void canConstruct() {
+        
+        final NotFoundException nfe = new NotFoundException();
+    }
+
+}
