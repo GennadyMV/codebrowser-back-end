222e045187010c839fab9e7912e78f4a45bdebe8
ljleppan <leo.leppanen@helsinki.fi>
2014-09-24 13:17:51 +0300

    Tests for DefaultStudentService

---
+++ src/test/java/fi/helsinki/cs/codebrowser/service/DefaultStudentServiceTest.java
@@ -0,0 +1,177 @@
+package fi.helsinki.cs.codebrowser.service;
+
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+
+import fi.helsinki.cs.codebrowser.app.App;
+import fi.helsinki.cs.codebrowser.exception.NotFoundException;
+import fi.helsinki.cs.codebrowser.model.Student;
+import fi.helsinki.cs.codebrowser.testutil.BackendServerStub;
+
+import java.io.IOException;
+import java.util.Collection;
+
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import static fi.helsinki.cs.codebrowser.testutil.BackendServerStub.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = App.class)
+@WebAppConfiguration
+@ActiveProfiles("test")
+public class DefaultStudentServiceTest {
+
+    @ClassRule
+    public static WireMockRule wireMockRule = new WireMockRule(8089);
+
+    private static final BackendServerStub SERVER = new BackendServerStub();
+
+    @Autowired
+    StudentService studentService;
+
+    @BeforeClass
+    public static void setUpClass() {
+
+        SERVER.initialiseServer();
+    }
+
+    private void assertOneHasUsername(final Collection<Student> students, final String username) {
+
+        for (Student student : students) {
+            if (student.getUsername().equals(username)) {
+                return;
+            }
+        }
+
+        fail("No element has name " + username);
+    }
+
+    @Test
+    public void findAllReturnsCorrectStudents() throws IOException {
+
+        final Collection<Student> students =  studentService.findAll(INSTANCE_ID);
+
+        assertEquals(2, students.size());
+        assertOneHasUsername(students, STUDENT_USERNAME);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void findAllThrowsNotFoundIfNoStudentsAreFound() throws IOException {
+
+        studentService.findAll(NO_SUCH_ID);
+    }
+
+    @Test
+    public void findAllByInstanceAndCourseReturnsCorrectStudents() throws IOException {
+
+        final Collection<Student> students = studentService.findAllBy(INSTANCE_ID, COURSE_ID);
+
+        assertEquals(1, students.size());
+        assertOneHasUsername(students, STUDENT_USERNAME);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void findAllByInstanceAndCourseThrowsNotFoundForNonexistantCourse() throws IOException {
+
+        studentService.findAllBy(INSTANCE_ID, NO_SUCH_ID);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void findAllByInstanceAndCourseThrowsNotFoundForNonexistantInstance() throws IOException {
+
+        studentService.findAllBy(NO_SUCH_ID, COURSE_ID);
+    }
+
+    @Test
+    public void findAllByInstanceCourseAndExerciseReturnsCorrectStudents() throws IOException {
+
+        final Collection<Student> students = studentService.findAllBy(INSTANCE_ID, COURSE_ID, EXERCISE_ID);
+
+        assertEquals(1, students.size());
+        assertOneHasUsername(students, STUDENT_USERNAME);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void findAllByInstanceCourseAndExerciseThrowsNotFoundForNonexistantInstance() throws IOException {
+
+        studentService.findAllBy(NO_SUCH_ID, COURSE_ID, EXERCISE_ID);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void findAllByInstanceCourseAndExerciseThrowsNotFoundForNonexistantCourse() throws IOException {
+
+        studentService.findAllBy(INSTANCE_ID, NO_SUCH_ID, EXERCISE_ID);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void findAllByInstanceCourseAndExerciseThrowsNotFoundForNonexistantExercise() throws IOException {
+
+        studentService.findAllBy(INSTANCE_ID, COURSE_ID, NO_SUCH_ID);
+    }
+
+    @Test
+    public void findReturnsCorrectStudent() throws IOException {
+
+        final Student student = studentService.find(INSTANCE_ID, COURSE_ID, EXERCISE_ID, STUDENT_ID);
+
+        assertEquals(STUDENT_ID, student.getId());
+        assertEquals(STUDENT_USERNAME, student.getUsername());
+        assertEquals(STUDENT_FIRSTNAME + " " + STUDENT_LASTNAME, student.getName());
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void findThrowsNotFoundForNonexistantInstance() throws IOException {
+
+        studentService.find(NO_SUCH_ID, COURSE_ID, EXERCISE_ID, STUDENT_ID);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void findThrowsNotFoundForNonexistantCourse() throws IOException {
+
+        studentService.find(INSTANCE_ID, NO_SUCH_ID, EXERCISE_ID, STUDENT_ID);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void findThrowsNotFoundForNonexistantExercise() throws IOException {
+
+        studentService.find(INSTANCE_ID, COURSE_ID, NO_SUCH_ID, STUDENT_ID);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void findThrowsNotFoundForNonexistantStudent() throws IOException {
+
+        studentService.find(INSTANCE_ID, COURSE_ID, EXERCISE_ID, NO_SUCH_ID);
+    }
+
+    @Test
+    public void findByInstanceReturnsCorrectStudent() throws IOException {
+
+        final Student student = studentService.findByInstance(INSTANCE_ID, STUDENT_ID);
+
+        assertEquals(STUDENT_ID, student.getId());
+        assertEquals(STUDENT_USERNAME, student.getUsername());
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void findByInstaceThrowsNotFoundForNonexistantInstance() throws IOException {
+
+        studentService.findByInstance(NO_SUCH_ID, STUDENT_ID);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void findByInstaceThrowsNotFoundForNonexistantStudent() throws IOException {
+
+        studentService.findByInstance(INSTANCE_ID, NO_SUCH_ID);
+    }
+}
--- src/test/java/fi/helsinki/cs/codebrowser/testutil/BackendServerStub.java
+++ src/test/java/fi/helsinki/cs/codebrowser/testutil/BackendServerStub.java
@@ -22,6 +22,9 @@ public final class BackendServerStub {
 
     public static final String STUDENT_USERNAME = "014000000";
     public static final String STUDENT_ID = "MDE0MDAwMDAw";
+    public static final String STUDENT_FIRSTNAME = "userFirstname";
+    public static final String STUDENT_LASTNAME = "userLastname";
+    public static final String STUDENT_EMAIL = "userEmail";
 
     public static final String EXERCISE_NAME = "WK1-W1E04.FirstHtmlForm";
     public static final String EXERCISE_ID = "V0sxLVcxRTA0LkZpcnN0SHRtbEZvcm0";
@@ -37,14 +40,21 @@ public final class BackendServerStub {
 
     public static final String COURSES_JSON_URL = "/hy/courses.json";
     public static final String COURSE_JSON_URL = "/hy/courses/26.json";
+    public static final String PARTICIPANTS_JSON_URL = "/hy/participants.json";
+    public static final String POINTS_JSON_URL = "/hy/courses/26/points.json";
+    public static final String EXERCISE_JSON_URL = "/hy/exercises/2112.json";
 
     public static final String COURSES_JSON = "{\"api_version\":7,\"courses\":[{\"id\":31,\"name\":\"k2014-ohja\",\"details_url\":\"http://tmc.mooc.fi/hy/courses/31.json\",\"unlock_url\":\"http://tmc.mooc.fi/hy/courses/31/unlock.json\",\"reviews_url\":\"http://tmc.mooc.fi/hy/courses/31/reviews.json\",\"comet_url\":\"http://tmc.mooc.fi:8080/comet\",\"spyware_urls\":[\"http://hy.spyware.testmycode.net/\"]},{\"id\":26,\"name\":\"s2013-wepa\",\"details_url\":\"http://tmc.mooc.fi/hy/courses/26.json\",\"unlock_url\":\"http://tmc.mooc.fi/hy/courses/26/unlock.json\",\"reviews_url\":\"http://tmc.mooc.fi/hy/courses/26/reviews.json\",\"comet_url\":\"http://tmc.mooc.fi:8080/comet\",\"spyware_urls\":[\"http://hy.spyware.testmycode.net/\"]}]}";
     public static final String COURSE_JSON = "{\"api_version\":7,\"course\":{\"id\":26,\"name\":\"s2013-wepa\",\"details_url\":\"http://tmc.mooc.fi/hy/courses/26.json\",\"unlock_url\":\"http://tmc.mooc.fi/hy/courses/26/unlock.json\",\"reviews_url\":\"http://tmc.mooc.fi/hy/courses/26/reviews.json\",\"comet_url\":\"http://tmc.mooc.fi:8080/comet\",\"spyware_urls\":[\"http://hy.spyware.testmycode.net/\"],\"unlockables\":[],\"exercises\":[{\"id\":2112,\"name\":\"WK1-W1E04.FirstHtmlForm\",\"locked\":false,\"deadline_description\":\"2013-09-11 23:59:00 +0300\",\"deadline\":\"2013-09-11T23:59:00+03:00\",\"checksum\":\"e41c0780ad6ffddcb12829e2c9f309a9\",\"return_url\":\"http://tmc.mooc.fi/hy/exercises/2112/submissions.json\",\"zip_url\":\"http://tmc.mooc.fi/hy/exercises/2112.zip\",\"returnable\":true,\"requires_review\":false,\"attempted\":false,\"completed\":false,\"reviewed\":false,\"all_review_points_given\":true,\"memory_limit\":null,\"runtime_params\":[],\"solution_zip_url\":\"http://tmc.mooc.fi/hy/exercises/2112/solution.zip\",\"exercise_submissions_url\":\"http://tmc.mooc.fi/hy/exercises/2112.json?api_version=7\"},{\"id\":2113,\"name\":\"WK1-W1E05.PageViewCounter\",\"locked\":false,\"deadline_description\":\"2013-09-11 23:59:00 +0300\",\"deadline\":\"2013-09-11T23:59:00+03:00\",\"checksum\":\"3136319ea5be1b33bbf105ef380f319c\",\"return_url\":\"http://tmc.mooc.fi/hy/exercises/2113/submissions.json\",\"zip_url\":\"http://tmc.mooc.fi/hy/exercises/2113.zip\",\"returnable\":true,\"requires_review\":false,\"attempted\":false,\"completed\":false,\"reviewed\":false,\"all_review_points_given\":true,\"memory_limit\":null,\"runtime_params\":[],\"solution_zip_url\":\"http://tmc.mooc.fi/hy/exercises/2113/solution.zip\",\"exercise_submissions_url\":\"http://tmc.mooc.fi/hy/exercises/2113.json?api_version=7\"}]}}";
+    public static final String PARTICIPANTS_JSON = "{\"api_version\":7,\"participants\":[{\"id\":1,\"username\":\"014000000\",\"email\":\"userEmail\",\"first_name\":\"userFirstname\",\"last_name\":\"userLastname\"},{\"id\":2,\"username\":\"secondUser\",\"email\":\"secondUserEmail\",\"first_name\":\"secondUserFirst\",\"last_name\":\"secondUserLast\"},{\"id\":3,\"username\":\"nope\",\"email\":\"noEmail\",\"first_name\":\"no\",\"last_name\":\"pe\"}]}";
+    public static final String POINTS_JSON = "{\"sheets\":[{\"name\":\"WK1\",\"total_awarded\":1107,\"total_available\":11},{\"name\":\"WK2\",\"total_awarded\":1262,\"total_available\":14},{\"name\":\"WK3\",\"total_awarded\":1153,\"total_available\":15},{\"name\":\"WK4\",\"total_awarded\":1269,\"total_available\":16},{\"name\":\"WK5\",\"total_awarded\":958,\"total_available\":13},{\"name\":\"WK6\",\"total_awarded\":762,\"total_available\":12}],\"total_awarded\":6511,\"total_available\":81,\"awarded_for_user_and_sheet\":{\"014000000\":{\"WK1\":11,\"WK2\":14,\"WK3\":15,\"WK4\":16,\"WK5\":13,\"WK6\":7}},\"total_for_user\":{\"014000000\":76},\"users\":[{\"administrator\":false,\"id\":1,\"login\":\"014000000\"}]}";
+    public static final String EXERCISE_JSON = "{\"course_name\":\"s2013-wepa\",\"course_id\":26,\"exercise_name\":\"WK1-W1E04.FirstHtmlForm\",\"exercise_id\":2112,\"unlocked_at\":null,\"deadline\":\"2013-09-11T23:59:00+03:00\",\"submissions\":[{\"exercise_name\":\"WK1-W1E04.FirstHtmlForm\",\"id\":139385,\"course_id\":26,\"user_id\":1,\"created_at\":\"2013-09-11T23:58:48+03:00\",\"all_tests_passed\":true,\"points\":\"W1E04\",\"submitted_zip_url\":\"a\",\"paste_url\":null,\"processing_time\":45,\"reviewed?\":false,\"requests_review?\":false},{\"exercise_name\":\"WK1-W1E04.FirstHtmlForm\",\"id\":139369,\"course_id\":26,\"user_id\":2,\"created_at\":\"2013-09-11T23:45:48+03:00\",\"all_tests_passed\":true,\"points\":\"W1E04\",\"submitted_zip_url\":\"a\",\"paste_url\":null,\"processing_time\":33,\"reviewed?\":false,\"requests_review?\":false}]}";
 
     // Snapshot API
 
     public static final String INSTANCES_URL = "/";
     public static final String INSTANCE_URL = "/hy";
+    public static final String STUDENTS_URL = "/hy/participants";
     public static final String STUDENT_URL = "/hy/participants/MDE0MDAwMDAw";
     public static final String STUDENT_COURSES_URL = "/hy/participants/MDE0MDAwMDAw/courses";
     public static final String STUDENT_COURSE_URL = "/hy/participants/MDE0MDAwMDAw/courses/czIwMTMtd2VwYQ";
@@ -58,6 +68,7 @@ public final class BackendServerStub {
 
     public static final String INSTANCES_JSON = "[{\"id\":\"hy\",\"name\":\"hy\"},{\"id\":\"mooc\",\"name\":\"mooc\"}]";
     public static final String INSTANCE_JSON = "{\"id\":\"hy\",\"name\":\"hy\"}";
+    public static final String STUDENTS_JSON = "[{\"id\":\"MDE0MDAwMDAw\",\"username\":\"014000000\"},{\"id\":\"c2Vjb25kVXNlcg==\",\"username\":\"secondUser\"}]";
     public static final String STUDENT_JSON = "{\"id\":\"MDE0MDAwMDAw\",\"username\":\"014000000\",\"courses\":[{\"id\":\"czIwMTMtd2VwYQ\",\"name\":\"s2013-wepa\",\"exercises\":[{\"id\":\"V0sxLVcxRTA0LkZpcnN0SHRtbEZvcm0\",\"name\":\"WK1-W1E04.FirstHtmlForm\"},{\"id\":\"V0sxLVcxRTA1LlBhZ2VWaWV3Q291bnRlcg\",\"name\":\"WK1-W1E05.PageViewCounter\"}]}]}";
     public static final String STUDENT_COURSES_JSON = "[{\"id\":\"czIwMTMtd2VwYQ\",\"name\":\"s2013-wepa\",\"exercises\":[{\"id\":\"V0sxLVcxRTA0LkZpcnN0SHRtbEZvcm0\",\"name\":\"WK1-W1E04.FirstHtmlForm\"},{\"id\":\"V0sxLVcxRTA1LlBhZ2VWaWV3Q291bnRlcg\",\"name\":\"WK1-W1E05.PageViewCounter\"}]}]";
     public static final String STUDENT_COURSE_JSON = "{\"id\":\"czIwMTMtd2VwYQ\",\"name\":\"s2013-wepa\",\"exercises\":[{\"id\":\"V0sxLVcxRTA0LkZpcnN0SHRtbEZvcm0\",\"name\":\"WK1-W1E04.FirstHtmlForm\"}]}";
@@ -92,10 +103,14 @@ public final class BackendServerStub {
         // TMC API
         stubGetWithJsonResponse(1, COURSES_JSON_URL, COURSES_JSON);
         stubGetWithJsonResponse(1, COURSE_JSON_URL, COURSE_JSON);
+        stubGetWithJsonResponse(1, PARTICIPANTS_JSON_URL, PARTICIPANTS_JSON);
+        stubGetWithJsonResponse(1, POINTS_JSON_URL, POINTS_JSON);
+        stubGetWithJsonResponse(1, EXERCISE_JSON_URL, EXERCISE_JSON);
 
         // Snapshot API
-        stubGetWithJsonResponse(12, INSTANCES_URL, INSTANCES_JSON);
-        stubGetWithJsonResponse(11, INSTANCE_URL, INSTANCE_JSON);
+        stubGetWithJsonResponse(13, INSTANCES_URL, INSTANCES_JSON);
+        stubGetWithJsonResponse(12, INSTANCE_URL, INSTANCE_JSON);
+        stubGetWithJsonResponse(11, STUDENTS_URL, STUDENTS_JSON);
         stubGetWithJsonResponse(10, STUDENT_URL, STUDENT_JSON);
         stubGetWithJsonResponse(9, STUDENT_COURSES_URL, STUDENT_COURSES_JSON);
         stubGetWithJsonResponse(8, STUDENT_COURSE_URL, STUDENT_COURSE_JSON);
