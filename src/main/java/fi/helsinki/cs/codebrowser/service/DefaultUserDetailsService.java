831e45de548c8263a66c148dfa8a98ebfe197708
ljleppan <leo.leppanen@helsinki.fi>
2014-09-29 16:27:04 +0300

    Added custom implementation of UserDetailsService

---
+++ src/main/java/fi/helsinki/cs/codebrowser/service/DefaultUserDetailsService.java
@@ -0,0 +1,51 @@
+package fi.helsinki.cs.codebrowser.service;
+
+import fi.helsinki.cs.codebrowser.model.Role;
+import fi.helsinki.cs.codebrowser.model.User;
+import fi.helsinki.cs.codebrowser.repository.UserRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class DefaultUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(final String username) {
+
+        final User user = userRepository.findByUsername(username);
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                true,
+                true,
+                true,
+                true,
+                getRolesAsGrantedAuthorities(user.getRoles()));
+
+    }
+
+    private List<GrantedAuthority> getRolesAsGrantedAuthorities(final List<Role> roles) {
+
+        final List<GrantedAuthority> authorities = new ArrayList<>();
+
+        for (Role role : roles) {
+            authorities.add(new SimpleGrantedAuthority(role.getRolename()));
+        }
+
+        return authorities;
+    }
+}
