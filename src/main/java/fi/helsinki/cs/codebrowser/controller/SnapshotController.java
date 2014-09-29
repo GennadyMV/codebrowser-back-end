871b607ef23e558b49275624a77129ecc4acf431
Joni Salmi <josalmi@cs.helsinki.fi>
2014-09-29 12:47:00 +0300

    Files from filtering default value change to empty string to avoid nulls

--- src/main/java/fi/helsinki/cs/codebrowser/controller/SnapshotController.java
+++ src/main/java/fi/helsinki/cs/codebrowser/controller/SnapshotController.java
@@ -53,7 +53,7 @@ public final class SnapshotController {
                                         @PathVariable final String courseId,
                                         @PathVariable final String exerciseId,
                                         @RequestParam(value = "level", defaultValue = "KEY", required = false) final String level,
-                                        @RequestParam(value = "from", required = false) final String from,
+                                        @RequestParam(value = "from", defaultValue = "", required = false) final String from,
                                         @RequestParam(value = "count", defaultValue = "0", required = false) final int count) throws IOException {
 
         return new HttpEntity<>(snapshotService.findFilesAsZip(instanceId, studentId, courseId, exerciseId, level, from, count));
