04e185417016ec08f671c04083b2aef0aceb86b6
ljleppan <leo.leppanen@helsinki.fi>
2014-10-01 13:34:18 +0300

    Tests for DefaultTokenService

---
+++ src/test/java/fi/helsinki/cs/codebrowser/service/DefaultTokenServiceTest.java
@@ -0,0 +1,78 @@
+package fi.helsinki.cs.codebrowser.service;
+
+import fi.helsinki.cs.codebrowser.app.App;
+import fi.helsinki.cs.codebrowser.model.User;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = App.class)
+@ActiveProfiles("test")
+@WebAppConfiguration
+public class DefaultTokenServiceTest {
+
+    @Autowired
+    private TokenService tokenService;
+
+    private User user;
+
+    @Before
+    public void setUp() {
+
+        user = new User();
+    }
+
+    @Test
+    public void generatesTokenForUser() {
+
+        final String token = tokenService.token(user);
+
+        assertNotNull(token);
+        assertTrue(token.length() > 0);
+    }
+
+    @Test
+    public void authenticateReturnsCorrectUserForToken() {
+
+        final String token = tokenService.token(user);
+
+        final User returnedUser = tokenService.authenticate(token);
+
+        assertEquals(user, returnedUser);
+    }
+
+    @Test
+    public void authenticateReturnsCorrectUserForTokenMultipleTimes() {
+
+        final String token = tokenService.token(user);
+
+        tokenService.authenticate(token);
+        final User returnedUser = tokenService.authenticate(token);
+
+        assertEquals(user, returnedUser);
+    }
+
+    @Test
+    public void creatingNewTokenInvalidatesPrevious() {
+
+        final String token = tokenService.token(user);
+        tokenService.token(user);
+
+        final User returnedUser = tokenService.authenticate(token);
+
+        assertNull(returnedUser);
+    }
+}
