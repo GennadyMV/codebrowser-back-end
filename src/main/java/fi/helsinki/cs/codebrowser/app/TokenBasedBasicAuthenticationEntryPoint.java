0b655deae76578ad02895b80dfb6eed6e5f02c6f
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2014-10-07 13:51:26 +0300

    Refactor

--- src/main/java/fi/helsinki/cs/codebrowser/app/TokenBasedBasicAuthenticationEntryPoint.java
+++ src/main/java/fi/helsinki/cs/codebrowser/app/TokenBasedBasicAuthenticationEntryPoint.java
@@ -6,14 +6,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
-public class TokenBasedBasicAuthenticationEntryPoint implements InitializingBean, AuthenticationEntryPoint {
-
-    @Override
-    public void afterPropertiesSet() throws Exception { }
+public final class TokenBasedBasicAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
     public void commence(final HttpServletRequest request,
