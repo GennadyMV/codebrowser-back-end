c7a0e5e6e98d2deff813b83417bed1629e0290a9
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2014-11-05 13:35:53 +0200

    Add tests for TokenBasedBasicAuthenticationFilterTest

---
+++ src/test/java/fi/helsinki/cs/codebrowser/app/filter/TokenBasedBasicAuthenticationFilterTest.java
@@ -0,0 +1,76 @@
+package fi.helsinki.cs.codebrowser.app.filter;
+
+import fi.helsinki.cs.codebrowser.app.App;
+import fi.helsinki.cs.codebrowser.model.User;
+import fi.helsinki.cs.codebrowser.repository.UserRepository;
+import fi.helsinki.cs.codebrowser.service.TokenService;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.mock.web.MockFilterChain;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = App.class)
+@WebAppConfiguration
+@ActiveProfiles("test")
+public final class TokenBasedBasicAuthenticationFilterTest {
+
+    private static final String AUTHENTICATION_TOKEN_HEADER = "X-Authentication-Token";
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private TokenBasedBasicAuthenticationFilter filter;
+
+    @Test
+    public void shouldNotReturnTokenIfUnauthorized() throws IOException, ServletException {
+
+        final MockHttpServletResponse response = new MockHttpServletResponse();
+
+        filter.doFilter(new MockHttpServletRequest(), response, new MockFilterChain());
+
+        assertNull(response.getHeader(AUTHENTICATION_TOKEN_HEADER));
+    }
+
+    @Test
+    public void shouldReturnTokenIfAuthorized() throws IOException, ServletException {
+
+        User user = new User();
+        user.setUsername("username");
+        user.setPassword("passwordpassword");
+
+        user = userRepository.save(user);
+
+        SecurityContextHolder.getContext()
+                             .setAuthentication(new UsernamePasswordAuthenticationToken("username", "passwordpassword"));
+
+        final MockHttpServletResponse response = new MockHttpServletResponse();
+
+        filter.doFilter(new MockHttpServletRequest(), response, new MockFilterChain());
+
+        userRepository.delete(user);
+
+        assertEquals(response.getHeader(AUTHENTICATION_TOKEN_HEADER), tokenService.token(user));
+    }
+}
